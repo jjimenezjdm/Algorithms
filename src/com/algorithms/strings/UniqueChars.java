5b1bd87dcc6976452e5d65c0e6ff3a487e6f14a1
jjimenezjdm <jjimenezjdm@gmail.com>
2020-04-14 19:54:16 +0200

    Unique chars problem

---
+++ src/com/algorithms/strings/UniqueChars.java
@@ -0,0 +1,26 @@
+package com.algorithms.strings;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class UniqueChars {
+
+    /**
+     * Implement an algorithm to determine if a string has all unique characters. What if you cannot
+     * use additional data structures?
+     */
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final String line = sc.nextLine();
+        HashSet<Character> wordsInLine = new HashSet<>();
+        boolean unique = true;
+        for (int i = 0; i < line.length() && unique; i++) {
+            if (wordsInLine.contains(line.charAt(i)))
+                unique = false;
+            else
+                wordsInLine.add(line.charAt(i));
+        }
+        System.out.println(unique);
+    }
+}
